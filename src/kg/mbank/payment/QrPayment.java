614b6e1c840985f8695288bb520baba79029a325
Ali <@.com>
2026-01-18 18:52:28 +0600

    added new massage

--- src/kg/mbank/payment/QrPayment.java
+++ src/kg/mbank/payment/QrPayment.java
@@ -3,5 +3,6 @@ package kg.mbank.payment;
 public class QrPayment {
     public static void main(String[] args) {
         System.out.println("Qr Payment");
+        System.out.println("Else");
     }
 }
